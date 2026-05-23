158c1034ba275f0b9d0516d913496c7855ee1ebd
joshiadobe <ja3129790@gmail.com>
2026-05-24 05:03:20 +0530

    Moved to OIDC Prod still not static ext id thou

--- auth-config.js
+++ auth-config.js
@@ -1,14 +1,19 @@
 export const AUTH_CONFIG = {
-  BACKEND_URL: "http://10.42.71.33:3000",
+  // BACKEND_URL: "http://10.42.71.33:3000",
+  BACKEND_URL: "http://orbit.corp.adobe.com",
 
   OKTA_AUTHORIZE_URL:
-    "https://adobe-stage.okta.com/oauth2/v1/authorize",
+    // "https://adobe-stage.okta.com/oauth2/v1/authorize",
+    "https://adobe.okta.com/oauth2/v1/authorize",
 
   OKTA_TOKEN_URL:
-    "https://adobe-stage.okta.com/oauth2/v1/token",
+    // "https://adobe-stage.okta.com/oauth2/v1/token",
+    "https://adobe.okta.com/oauth2/v1/token",
 
   CLIENT_ID:
-    "0oa235m80hlSgh7is1d8",
+    // "0oa235m80hlSgh7is1d8",
+    "0oa2752jqqwCvPkvS0h8",
+
 
   SCOPES:
     "openid profile email offline_access",
